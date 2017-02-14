21e4ad931010aa05ea94e302e3a4a5722aa973be
Paulson Li <paulsonli99@gmail.com>
2017-02-14 22:03:48 +0000

    house

--- script.js
+++ script.js
@@ -14,5 +14,31 @@ forward(50);
 
 // Type your function call below
 
+// square();
 
-stamp();
+function triangle(x){
+    forward(x);
+    right(120);
+    forward(x);
+    right(120);
+    forward(x);
+}
+
+
+function house(){
+    right(180);
+    forward(30);
+    left(90);
+    forward(50);
+    left(90);
+    forward(30);
+    left(45);
+    forward(35);
+    left(90);
+    forward(36);
+    left(135 );
+    forward(50);
+    
+}
+house();
+//stamp();
